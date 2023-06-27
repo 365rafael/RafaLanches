a227f5dce157e31dc4b5331e01eb55618111616c
Rafael Arantes <rafael-fullstack@outlook.com>
2023-06-27 15:09:37 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RafaLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
