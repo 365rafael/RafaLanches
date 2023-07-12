526da01178fc28b65af90954b552dc62214580fe
Rafael Arantes <rafael-fullstack@outlook.com>
2023-07-11 21:18:39 -0300

    Data Anotacions

--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -8,8 +8,10 @@ namespace RafaLanches.Models
     {
         [Key]
         public int LancheId { get; set; }
+
         [Required(ErrorMessage ="O nome do lanche deve ser informado")]
         [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength =10, ErrorMessage ="O {0} deve ter no mínimo {1} e no máximo {2}")]
         public string Name { get; set; }
 
         [Required(ErrorMessage ="A descrição do lanche deve ser informada")]
@@ -17,13 +19,31 @@ namespace RafaLanches.Models
         [MinLength(20, ErrorMessage ="Descrição deve ter no mínimo {1} caracteres")]
         [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} caracteres")]
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
 
+        [Required(ErrorMessage ="Irforme o preço do lanche")]
+        [Display(Name ="Preço")]
+        [Column(TypeName="decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage ="o preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho da imagem normal")]
+        [StringLength(200, ErrorMessage ="O {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho da imagem miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
 
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; set; }
+
+        [Display(Name ="Estoque?")]
         public bool EmEstoque { get; set; }
 
         public int CategoriaId { get; set; }
